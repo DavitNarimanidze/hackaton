cae712ab2f5426fd95962bc4543dca316fd2b914
DavitNarimanidze <davitnarimanidze58@gmail.com>
2024-12-01 18:37:28 +0400

    second commit

--- n17.py
+++ n17.py
@@ -1,3 +1,5 @@
+# 17 შექმენით manual_len() ფუნქცია. ანუ უნდა შექმნათ ისეთი ფუნქცია რომელიც დათვლის რამდენი ელემენტია რაიმე list'ში ან რამდენი სიმბოლოა რაიმე string'ში.												
+
 listi = ["hi", "bye", "How are you"]
 def manual_len():
     varuable = input("Print name of your varuable or list: ")
