920d306d2690c425177c6e6fc70b9f1cc5495345
DavitNarimanidze <davitnarimanidze58@gmail.com>
2024-12-01 18:32:28 +0400

    second commit

---
+++ n17.py
@@ -0,0 +1,6 @@
+listi = ["hi", "bye", "How are you"]
+def manual_len():
+    varuable = input("Print name of your varuable or list: ")
+    lenght = len(varuable)
+    print("Length of your varuable or list is: ", lenght)
+manual_len()
